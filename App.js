6c9f83c3e42ba6f13debd3c017971cc1c0caf42b
Piyush Dhimate <bitartist@hotmail.com>
2020-01-15 22:03:14 +1100

    Used a prop in a component

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Greeting } from "./components/prop";
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ alignItems: 'center', top: 50 }}>
         <Text>Hello, world!</Text>
+        <Greeting name="Piyush"></Greeting>
+        <Greeting name="Piyush2"></Greeting>
+        <Greeting name="Piyush3"></Greeting>
       </View>
     );
   }
