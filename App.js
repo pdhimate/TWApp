5ac7f4645a4fc4e4ebfee595730517441ddf69ba
Piyush Dhimate <bitartist@hotmail.com>
2020-01-15 22:57:05 +1100

    Used flex for styling

--- App.js
+++ App.js
@@ -3,17 +3,20 @@ import { Text, View } from 'react-native';
 import { Greeting } from "./components/prop";
 import { Blinker } from "./components/blinker";
 import { textStyles } from "./styles/textStyles";
+import { backgrounds } from "./styles/backgroundStyles";
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ alignItems: 'center', top: 50 }}>
+      <View style={{  flex: 1 }}>
         <Text>Hello, world!</Text>
         <Greeting name="Piyush"></Greeting>
         <Greeting name="Piyush2"></Greeting>
         <Text style={textStyles.red}>Piyush styled</Text>
         <Text style={textStyles.boldBlue}>Piyush styled2</Text>
         <Blinker text="Welcome!!!"></Blinker>
+        <Text style={backgrounds.smallBlue}>Text with background</Text>
+        <Text style={backgrounds.mediumBlue}>Text with background2</Text>
       </View>
     );
   }
---
+++ styles/backgroundStyles.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const backgrounds = StyleSheet.create({
+    smallBlue: {
+        backgroundColor: 'powderblue',
+        flex: 1
+    },
+    mediumBlue: {
+        backgroundColor: 'skyblue',
+        flex: 2
+    },
+});
